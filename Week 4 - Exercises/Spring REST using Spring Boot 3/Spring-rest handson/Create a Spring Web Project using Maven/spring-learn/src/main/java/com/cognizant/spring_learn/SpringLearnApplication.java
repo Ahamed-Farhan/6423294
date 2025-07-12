27f860760ec0314a3c23ff9caef3f6a32270c7a3
Ahamed-Farhan <119913725+Ahamed-Farhan@users.noreply.github.com>
2025-07-12 18:21:23 +0530

    Week - 4 Hands-on Achieved

---
+++ Week 4 - Exercises/Spring REST using Spring Boot 3/Spring-rest handson/Create a Spring Web Project using Maven/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,20 @@
+package com.cognizant.spring_learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties.UiService.LOGGER;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		log.info("Inside Main");
+	}
+
+}
