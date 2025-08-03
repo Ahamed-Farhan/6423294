53b89c6da51d82475bf323a7c600bfaf93ef6c33
Ahamed-Farhan <ahamedfarhan155@gmail.com>
2025-08-03 16:18:18 +0530

    Week 7 Third Exercise

---
+++ Week 7 - Exercises/11. ReactJS-HOL/eventexamplesapp/src/App.js
@@ -0,0 +1,16 @@
+
+import { Counter } from './Components/Counter';
+import { CurrencyConvertor } from './Components/CurrentConvertor';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Counter/>
+      <br/>
+      <CurrencyConvertor/>
+    </div>
+  );
+}
+
+export default App;
